7a6c59a7cd9e43d5fefcfc54db9b38dc76d9bad1
Edgar Fabian Molina Fabela <133904881+MolinaEdgar@users.noreply.github.com>
2024-11-18 21:28:59 -0800

    Create Ejercicio15.s

---
+++ Ejercicio15.s
@@ -0,0 +1,13 @@
+***************************************************************
+*                                                             *
+*                Instituto Tecnico De Tijuana                 *
+*                                                             *
+*                  Edgar Fabian Molina Fabela                 *
+*                                                             *
+*                 Número de Control: 22210780                 *
+*                                                             *
+*                      Fecha: 19/11/24                        *
+*                                                             *
+*                  Proyecto: 50 Programas AWS                 *
+*                                                             *
+***************************************************************
